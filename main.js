661416c137343a2322b61192ad88e62c0d8d1f27
diyorbek1010 <ataboyevdiyorbek1010@gmail.com>
2021-03-28 10:24:10 +0500

    aded text color

--- main.js
+++ main.js
@@ -1,17 +1,35 @@
-const decroment= document.querySelector('#decroment');
-const incroment= document.querySelector('#incroment');
-const text= document.querySelector('h1');
-let count=0
+const decroment = document.querySelector("#decroment");
+const incroment = document.querySelector("#incroment");
+const text = document.querySelector("h1");
+let count = 0;
 
 text.innerHTML = count;
-decroment.addEventListener('click', delHandler);
-incroment.addEventListener('click',addHandler);
+decroment.addEventListener("click", delHandler);
+incroment.addEventListener("click", addHandler);
 
-function delHandler(){
-    count--;
-    text.innerHTML = count;
+function delHandler() {
+  count--;
+  if (count == 0) {
+    text.style.color = "black";
+  }
+  if (count < 0) {
+    text.style.color = "red";
+  }
+  if (count > 0) {
+    text.style.color = "green";
+  }
+  text.innerHTML = count;
 }
-function  addHandler(){
-    count++;
-    text.innerHTML = count;
+function addHandler() {
+  count++;
+  if (count == 0) {
+    text.style.color = "black";
+  }
+  if (count < 0) {
+    text.style.color = "red";
+  }
+  if (count > 0) {
+    text.style.color = "green";
+  }
+  text.innerHTML = count;
 }
